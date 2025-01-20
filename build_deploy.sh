36c3056baed1b41303e4da17c6c894993ece8cc5
Antony Natale <anatale@redhat.com>
2025-01-20 14:35:55 -0500

    fixes bad image def

--- build_deploy.sh
+++ build_deploy.sh
@@ -3,7 +3,7 @@
 set -exv
 
 if [[ -z "$IMAGE" ]]; then
-    IMAGE="quay.io/cloudservices/insights-kafka-connect"
+    IMAGE="quay.io/cloudservices/kafka-relations-sink"
 fi
 IMAGE_TAG=$(git rev-parse --short=7 HEAD)
 SECURITY_COMPLIANCE_TAG="sc-$(date +%Y%m%d)-$(git rev-parse --short=7 HEAD)"
